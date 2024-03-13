218755937fc65a5433a8cf8d1e9539c8384c6afd
atierz <atl_alicia96@hotmail.com>
2024-03-13 12:12:11 +0100

    add rmse_inf_tensor function

--- amb/metrics.py
+++ amb/metrics.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 
 def rmse_inf(data_ground_truth, data_predicted):
@@ -38,6 +39,41 @@ def rmse_inf(data_ground_truth, data_predicted):
     mse_inf = np.mean(se_inf)
     # Square root of the mean squared error
     rmse_inf = np.sqrt(mse_inf)
+
+    return rmse_inf
+
+
+def rmse_inf_tensor(data_ground_truth, data_predicted):
+    """
+    Calculate the Root Mean Squared Error (RMSE) normalized by the infinity norm.
+    Args:
+        data_ground_truth (torch.Tensor): Ground truth data with shape (snapshot, nodes, variable).
+                                           In case of (nodes, variable), it expands snapshot dim to 1.
+        data_predicted (torch.Tensor): Predicted data with the same shape as data_ground_truth.
+                                        In case of (nodes, variable), it expands snapshot dim to 1.
+    Returns:
+        float: RMSE normalized by the infinity norm.
+    """
+    if data_ground_truth.ndim == 2:
+        data_ground_truth = data_ground_truth[None, :, :]
+        data_predicted = data_predicted[None, :, :]
+    # squared error
+    se = (data_ground_truth - data_predicted)**2
+    # mean across number of variables
+    se = torch.mean(se, axis=-1)
+    # inifnite norm
+    se_inf = []
+    for i in range(data_ground_truth.shape[0]):
+        x_snap = data_ground_truth[i]
+        se_snap = se[i]
+        infinite_norm_se = torch.max((x_snap**2))
+        se_inf.append(se_snap / infinite_norm_se)
+    # se_inf = torch.array(se_inf)
+    se_inf = torch.cat(se_inf, dim=0)
+    # mean across all nodes
+    mse_inf = torch.mean(se_inf)
+    # squared root of the mse
+    rmse_inf = torch.sqrt(mse_inf)
 
     return rmse_inf
 
